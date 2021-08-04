bc05a5ff25d4febc1a10c1d163d8b0fec0df8ecc
jr8ppg <jr8ppg@gmail.com>
2021-08-04 21:18:51 +0900

    Changed: ScoreウインドウはQSO数のみの表示にする Issues #6

--- USimpleStats.pas
+++ USimpleStats.pas
@@ -13,9 +13,10 @@ type
   private
     { Private declarations }
   public
+    { Public declarations }
     LowBand, HighBand : TBand;
     procedure UpdateStats; override;
-   { Public declarations }
+    procedure InitGrid(LBand, HBand: TBand); override;
   end;
 
 implementation
@@ -24,14 +25,12 @@ implementation
 
 procedure TSimpleStats.UpdateStats;
 var
-   i, _totalqso, _totalmulti, _totalcw, _totalph, _totalpts: integer;
+   i, _totalqso, _totalcw, _totalph: integer;
    temp: string;
    B: TBand;
    R: double;
 begin
    _totalqso := 0;
-   _totalpts := 0;
-   _totalmulti := 0;
    _totalcw := 0;
    _totalph := 0;
    i := 1;
@@ -39,39 +38,32 @@ begin
    for B := LowBand to HighBand do begin
       if NotWARC(B) then begin
          Grid.Cells[1, i] := IntToStr(StatSummary[B].qso);
-         Grid.Cells[2, i] := IntToStr(StatSummary[B].points);
-         Grid.Cells[3, i] := IntToStr(StatSummary[B].multi1);
-         Grid.Cells[4, i] := IntToStr(StatSummary[B].cwqso);
-         Grid.Cells[5, i] := IntToStr(StatSummary[B].noncwqso);
+         Grid.Cells[2, i] := IntToStr(StatSummary[B].cwqso);
+         Grid.Cells[3, i] := IntToStr(StatSummary[B].noncwqso);
          if StatSummary[B].qso = 0 then
             R := 0
          else
             R := 100.0 * (StatSummary[B].cwqso) / (StatSummary[B].qso);
 
          temp := Format('%3.1f', [R]);
-         Grid.Cells[6, i] := temp;
+         Grid.Cells[4, i] := temp;
 
          inc(_totalqso, StatSummary[B].qso);
-         inc(_totalpts, StatSummary[B].points);
-         inc(_totalmulti, StatSummary[B].multi1);
          inc(_totalcw, StatSummary[B].cwqso);
          inc(_totalph, StatSummary[B].noncwqso);
          inc(i);
       end;
    end;
    Grid.Cells[1, i] := IntToStr(_totalqso);
-   Grid.Cells[2, i] := IntToStr(_totalpts);
-   Grid.Cells[3, i] := IntToStr(_totalmulti);
-   Grid.Cells[4, i] := IntToStr(_totalcw);
-   Grid.Cells[5, i] := IntToStr(_totalph);
+   Grid.Cells[2, i] := IntToStr(_totalcw);
+   Grid.Cells[3, i] := IntToStr(_totalph);
    if _totalqso = 0 then
       R := 0
    else
       R := 100 * _totalcw / _totalqso;
 
    temp := Format('%3.1f', [R]);
-   Grid.Cells[6, i] := temp;
-   Grid.Cells[3, i + 1] := IntToStr(_totalpts * _totalmulti);
+   Grid.Cells[4, i] := temp;
 end;
 
 procedure TSimpleStats.FormCreate(Sender: TObject);
@@ -88,6 +80,43 @@ begin
    UsedBands[b21] := True;
    UsedBands[b28] := True;
    UsedBands[b50] := True;
+end;
+
+procedure TSimpleStats.InitGrid(LBand, HBand: TBand);
+var
+   i: integer;
+   b: TBand;
+begin
+   with Grid do begin
+      i := 0;
+      for b := LBand to HBand do
+         if NotWARC(b) then
+            inc(i);
+
+      i := i + 3;
+      RowCount := i;
+
+      ColCount := 5;
+      Cells[0, 0] := 'MHz';
+      Cells[1, 0] := 'QSOs';
+      Cells[2, 0] := 'CW';
+      Cells[3, 0] := 'Ph';
+      Cells[4, 0] := 'CW%';
+
+      i := 1;
+      for b := LBand to HBand do begin
+         if NotWARC(b) then begin
+            Cells[0, i] := MHzString[b];
+            inc(i);
+         end;
+      end;
+
+      Cells[0, i] := 'Total';
+      Cells[0, i + 1] := 'Score';
+   end;
+
+   ClientHeight := Grid.DefaultRowHeight * Grid.RowCount + 2;
+   ClientWidth := Grid.DefaultColWidth * Grid.ColCount + 2;
 end;
 
 end.
